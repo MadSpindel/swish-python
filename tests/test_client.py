8e7953ff5491a6e8bb9622edaf23e8cfc22d4b1a
Fredrik Sundqvist <fsundqvist@gmail.com>
2016-02-09 23:24:16 +0100

    WIP

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,6 +1,9 @@
+import responses
 import unittest
 
 import swish
+
+LOCATION = 'https://swicpc.bankgirot.se/swishcpcapi/api/v1/paymentrequests/AB23D7406ECE4542A80152D909EF9F6B'
 
 
 class SwishClientTestCase(unittest.TestCase):
@@ -12,11 +15,26 @@ class SwishClientTestCase(unittest.TestCase):
         self.assertEqual(self.client.payee_alias, 'fake-alias')
         self.assertEqual(self.client.cert, 'fake-cert')
 
+    @responses.activate
     def test_payment_request(self):
+        def request_callback(request):
+            headers = {
+                'Location': LOCATION
+            }
+            return (201, headers)
+
+        responses.add_callback(
+            responses.POST,
+            self.client.environment.base_url + 'paymentrequests',
+            callback=request_callback,
+            content_type='application/json'
+        )
+
         self.client.payment_request(
             amount=1, currency='SEK', callback_url='https://fake-url.com/', payment_reference='fake', message='fake'
         )
         self.fail("Not implemented!")
 
+    @responses.activate
     def test_refund(self):
         self.fail("Not implemented!")
