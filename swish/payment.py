ed5b3e456a785dbc193c7cf0adddf743c1546f3c
Fredrik Sundqvist <fsundqvist@gmail.com>
2016-04-04 17:44:26 +0200

    Make request_token optional

--- swish/payment.py
+++ swish/payment.py
@@ -1,6 +1,6 @@
 
 
 class Payment(object):
-    def __init__(self, id, request_token):
+    def __init__(self, id, request_token=None):
         self.id = id
         self.request_token = request_token
